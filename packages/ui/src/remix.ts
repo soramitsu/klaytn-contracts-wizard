7098734182fa6a29c2c91d73603e59bfaa54e6c7
RDMStreet <RDMStreetOne@gmail.com>
2022-08-11 17:59:44 +0300

    [fix]: changed remix

--- packages/ui/src/remix.ts
+++ packages/ui/src/remix.ts
@@ -1,5 +1,5 @@
 export function remixURL(code: string, upgradeable = false): URL {
-  const remix = new URL('https://remix.ethereum.org');
+  const remix = new URL('https://ide.klaytn.foundation');
   remix.searchParams.set('code', btoa(code).replace(/=*$/, ''));
   if (upgradeable) {
     remix.searchParams.set('deployProxy', upgradeable.toString());
